b606fbb4456acc34faba2f77cf277519e21e3df6
Guy Bryant <guy.s.bryant@gmail.com>
2018-08-09 14:30:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,5 @@ var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value){
   var newObj = Object.assign({}, object, key.value);
-  console.log(newObj);
   return newObj;
-  
 }
-
-updateObjectWithKeyAndValue();
